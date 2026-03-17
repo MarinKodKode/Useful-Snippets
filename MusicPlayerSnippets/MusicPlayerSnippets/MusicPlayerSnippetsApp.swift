c9a9c05cd8cb7f877808b36ffd7462d1c637c05f
Manuel Alejandro Hernandez Marin <mhernandezm@inbursa.com>
2026-03-17 16:58:58 -0600

    [init] - MusicPlayerCreated
    Created Folder for MusicPlayerSnippets

---
+++ MusicPlayerSnippets/MusicPlayerSnippets/MusicPlayerSnippetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerSnippetsApp.swift
+//  MusicPlayerSnippets
+//
+//  Created by Manuel Alejandro Hernandez Marín on 17/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerSnippetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicPlayerSnippets/MusicPlayerSnippetsTests/MusicPlayerSnippetsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerSnippetsTests.swift
+//  MusicPlayerSnippetsTests
+//
+//  Created by Manuel Alejandro Hernandez Marín on 17/03/26.
+//
+
+import Testing
+@testable import MusicPlayerSnippets
+
+struct MusicPlayerSnippetsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
